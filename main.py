80ee4eb36b5aa55536ef934681e0426625098b77
autorip <AutoRip@outlook.com>
2020-07-22 00:23:23 +0800

    Update the URL entry to login successfully

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import json
 from lxml import etree
 import urllib.parse
 
-loginURL = "https://newsso.shu.edu.cn/login"
-reportURL = "https://selfreport.shu.edu.cn/XueSFX/HalfdayReport.aspx?t=1"
-homeURL = "https://selfreport.shu.edu.cn"
 # post
 
 class postBody(object):
@@ -18,17 +15,19 @@ class postBody(object):
                         temperature="36.2",
                         QRCodeColor="绿色", 
                         meal=["早餐", "中餐", "晚餐"],
-                        timeMark=""):
+                        timeMark="上午" if time.strftime("%p", time.localtime())=="AM" else "下午",
+                        viewstate=None,
+                        viewstategenerator=None):
         self.date = date
         self.bodyStatus = bodyStatus
         self.symptom = symptom
         self.temperature = temperature
         self.QRCodeColor = QRCodeColor
         self.meal = meal
-        self.timeMark = "上午" if timeMark else "下午"
+        self.timeMark = timeMark
+        self.viewstate = viewstate
+        self.viewstategenerator = viewstategenerator
         self.f_state = None
-        self.viewstate = None
-        self.viewstategenerator = None
         self.submitData = None
 
     def set(self):
@@ -80,8 +79,8 @@ class postBody(object):
             }
         }
 
-
     def get(self):
+        self.set()
         self.submitData = {
             "__EVENTTARGET": "p1$ctl00$btnSubmit",
             "__EVENTARGUMENT": "",
@@ -96,67 +95,60 @@ class postBody(object):
             "p1$ShiFJC": self.meal[2],
             "F_TARGET": "p1_ctl00_btnSubmit",
             "p1_Collapsed": "false",
-            "F_STATE": base64.b64encode(json.dumps(self.F_STATE).encode)
+            "F_STATE": base64.b64encode(json.dumps(self.f_state).encode())
         }
+        return self.submitData
 
 class AutoReport(object):
 
-    def __init__(self, id, password, timeMark):
+    loginURL = "https://newsso.shu.edu.cn/login"
+    reportURL = "https://selfreport.shu.edu.cn/XueSFX/HalfdayReport.aspx?t=%s"
+    homeURL = "https://selfreport.shu.edu.cn"
+    timeMarkDict = {"晨报": 1, "晚报": 2}
+
+    def __init__(self, id, password, tempreture):
         self.session = requests.Session()
         self.id = id
         self.password = password
-        self.timeMark = timeMark
+        self.tempreture = tempreture
+        # self.timeMark = timeMark
     
     def _login(self):
-        self.session.headers.update({"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"})
-        self.session.headers.update({"Accept-Language": "zh-cn"})
-        self.session.headers.update({"Accept-Encoding": "gzip, deflate, br"})
         self.session.headers.update({"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"})
-        self.session.headers.update({"Connection": "keep-alive"})
-        self.session.headers.update({"Host": "newsso.shu.edu.cn"})
-
-        # retrieve cookie
-        login = self.session.get(loginURL)
-
-        # post data
         data = {
             "username": self.id,
             "password": self.password,
             "login_submit": "登录/Login"
         }
-        self.session.headers.update({"Content-Type": "application/x-www-form-urlencoded"})
-        self.session.headers.update({"Referer": "https://newsso.shu.edu.cn/login"})
-        self.session.headers.update({"Origin": "https://newsso.shu.edu.cn"})
-        loginPost = self.session.post(loginURL, data=urllib.parse.urlencode(data), timeout=10, allow_redirects=False)
-        self.session.headers.pop("Content-Type")
-        self.session.headers.pop("Origin")
-        loginPostRedirect = self.session.get("https://newsso.shu.edu.cn"+loginPost.headers["Location"], timeout=10)
-        self.session.headers.pop("Referer")
+        self.session.post(self.loginURL, data=data)
+        self.session.get("https://newsso.shu.edu.cn/oauth/authorize?response_type=code&client_id=WUHWfrntnWYHZfzQ5QvXUCVy&redirect_uri=https%3a%2f%2fselfreport.shu.edu.cn%2fLoginSSO.aspx%3fReturnUrl%3d%252fDefault.aspx&scope=1")
+        # self.session.get("https://newsso.shu.edu.cn/oauth/authorize?response_type=code&client_id=WUHWfrntnWYHZfzQ5QvXUCVy&redirect_uri=https%3a%2f%2fselfreport.shu.edu.cn%2fLoginSSO.aspx%3fReturnUrl%3d%252FXueSFX%252FHalfdayReport.aspx%253Ft%253D1%26t%3d1&scope=1")
+        return 1
 
-        self.session.headers.update({"Host": "selfreport.shu.edu.cn"})
-        mainpage1 = self.session.get(homeURL+"/", allow_redirects=False)
-        mainpage2 = self.session.get(homeURL+mainpage1.headers["Location"], allow_redirects=False)
-        self.session.headers.update({"Host": "newsso.shu.edu.cn"})
-        mainpage3 = self.session.get(mainpage2.headers["Location"], allow_redirects=False)
-        self.session.headers.update({"Host": "selfreport.shu.edu.cn"})
-        mainpage4 = self.session.get(mainpage3.headers["Location"])
-        # resp3 = self.session.get("https://selfreport.shu.edu.cn/XueSFX/HalfdayReport.aspx?t=1", timeout=10)
+    def _generateData(self):
+        self._login()
+        URL = self.reportURL % ("1" if time.strftime("%p", time.localtime())=="AM" else "2")
+        resp = self.session.get(URL)
         html = etree.HTML(resp.text)
-        viewstate = html.xpath('string(//input[@id="__viewstate"]/@value)')
-        viewstategenerator = html.xpath('string(//input[@id="__viewstategenerator"]/@value')
-        return
+        viewstate = html.xpath('string(//input[@id="__VIEWSTATE"]/@value)')
+        viewstategenerator = html.xpath('string(//input[@id="__VIEWSTATEGENERATOR"]/@value)')
+        submit_data = postBody(temperature=self.tempreture, viewstate=viewstate, viewstategenerator=viewstategenerator).get()
+        return submit_data
 
-    def generateData(self):
-        resp = self.session.get("https://selfreport.shu.edu.cn/XueSFX/HalfdayReport.aspx?t=2")
-        html = etree.HTML(resp.text)
-        viewstate = html.xpath('string(//input[@id="__viewstate"]/@value)')
-        viewstategenerator = html.xpath('string(//input[@id="__viewstategenerator"]/@value')
-        return
+    def _postData(self):
+        data = self._generateData()
+        self.session.headers.update({"X-Requested-With": "XMLHttpRequest"})
+        self.session.headers.update({"X-FineUI-Ajax": "true"})
+        self.session.headers.update({"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"})
+        URL = self.reportURL % ("1" if time.strftime("%p", time.localtime()) == "AM" else "2")
+        resp = self.session.post(URL, data=data)
+        return 1
 
-    def postData(self):
-        pass
+    def do(self):
+        self._postData()
+        return 1
         
 
 if __name__ == "__main__":
-    automation = AutoReport(sys.argv[1], sys.argv[2], 1)
-    automation._login()
+    automation = AutoReport(sys.argv[1], sys.argv[2], sys.argv[3])
+    automation.do()
